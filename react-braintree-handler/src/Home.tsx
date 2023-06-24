46b6551c9e476d8830c9b95d4110a1f60b259b1d
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-24 11:13:05 +0100

    adjustments

--- react-braintree-handler/src/Home.tsx
+++ react-braintree-handler/src/Home.tsx
@@ -22,17 +22,23 @@ const Home: React.FC = () => {
   const [dropinInstance, setDropinInstance] = useState<Dropin | null>(null);
 
   useEffect(() => {
+    console.log('Fetching client token');
     axios.get('https://candii4-backend2-3f9abaacb350.herokuapp.com/client_token')
       .then(response => {
+        console.log('Client token response', response);
         setClientToken(response.data.clientToken);
       })
       .catch((error: ErrorResponse) => {
         console.error("Error fetching client token:", error.message);
       });
   }, []);
+  
 
   useEffect(() => {
+    console.log('useEffect triggered for clientToken:', clientToken);
+    
     if (clientToken && dropinContainer.current) {
+      console.log('Creating Braintree instance');
       braintree.create({
         authorization: clientToken,
         container: dropinContainer.current,
@@ -41,26 +47,52 @@ const Home: React.FC = () => {
           console.error('Create error', createErr);
           return;
         }
+  
+        console.log('Setting dropin instance');
         setDropinInstance(instance);
-        // Immediately request the payment method
-        instance.requestPaymentMethod()
+      });
+    } else {
+      console.log('Client token or container is not ready');
+    }
+  }, [clientToken]);
+  
+
+  const handlePaymentRequest = () => {
+    console.log('Starting payment request');
+  
+    if(dropinInstance) {
+      console.log('Dropin instance found, requesting payment method');
+  
+      dropinInstance.requestPaymentMethod()
         .then(({ nonce }: NoncePayload) => {
+          console.log('Nonce received', nonce);
+          console.log('Sending nonce to server for payment processing');
+  
           return axios.post('https://candii4-backend2-3f9abaacb350.herokuapp.com/payment', { nonce });
         })
         .then((response: any) => {
           // Handle response
-          console.log('Payment response:', response.data);
+          console.log('Payment response received from server:', response.data);
         })
-        .catch((error: ErrorResponse) => {
-          // Handle error
-          console.error('Payment error:', error.message);
+        .catch((error: any) => {
+          if (error.message) {
+            console.error('Payment error:', error.message);
+          } else {
+            console.error('Error during requestPaymentMethod:', error);
+          }
         });
-      });
+    } else {
+      console.error('DropinInstance is null, unable to start payment process');
     }
-  }, [clientToken]);
+  };
+  
 
+  
   return (
-    <div ref={dropinContainer} />
+    <div>
+      <div ref={dropinContainer} />
+      <button onClick={handlePaymentRequest}>Pay</button>
+    </div>
   );
 };
 
